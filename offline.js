407c667bebbbe724c90bb1ef6da97b1e9b4f85a2
RAHMAADENAN <rahmaadenan561@gmail.com>
2025-12-14 16:30:51 -0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1765812141,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/jembatan-sheet0.png",
+		"images/karakterutama-sheet0.png",
+		"images/kamera-sheet0.png",
+		"images/cooin-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/karakter__utama-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coin-sheet1.png",
+		"images/batu-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
